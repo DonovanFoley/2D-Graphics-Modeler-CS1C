b4319b6069c51d02ad1010253baabacfa7fa1837
Sierra Martin <86510695+mcdipples@users.noreply.github.com>
2021-11-11 16:22:18 -0800

    Add files via upload
    Created first window user encounters - login screen. Username = user, Password = pass. For admins, username is admin and password is pass. After login, second dialog window pops up. This eventually will become the main window for our shapes.

---
+++ mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+// for testing rn
+void MainWindow::on_pushButton_login_clicked()
+{
+    QString username = ui->lineEdit_username->text();
+    QString password = ui->lineEdit_password->text();
+
+    if (username == "user" && password == "pass")
+    {
+        QMessageBox::information(this, "Login", "Username and password are correct.");
+        hide(); // hides login window upon successful login
+        SecDialog = new secDialog(this);
+        SecDialog->show();
+    }
+    if (username == "admin" && password == "pass")
+    {
+        QMessageBox::information(this, "Login", "Admin login enabled.");
+        adminLog = true;    // will be used later in order to enable admin privileges
+        hide();
+        SecDialog = new secDialog(this);
+        SecDialog->show();
+    }
+    else
+    {
+        QMessageBox::warning(this, "Login", "Incorrect username and password!");
+    }
+}
+
