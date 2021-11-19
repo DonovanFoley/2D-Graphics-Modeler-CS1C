0fb613b5cdc11e7cafa8498672ae7c531f978edb
Roift <isaac@crazyorganic.net>
2021-11-18 17:08:40 -0800

    Update parser.cpp
    new method of data parsing i was working on, currently non-functional but i think im on the right track with the logic
    
    it's messy bc i'm neglecting to make read functions for each shape and rather just doing it all in the main parsing function

--- parser.cpp
+++ parser.cpp
@@ -10,27 +10,27 @@
 
 const int BLOCK_MAX = 500;
 
-void sdParser(QString filename) {
+struct Data {
+    int shapeID;
+    QString shapeType;
+    int shapeDimensions[10];
+    QString penColor;
+    int penWidth;
+    QString penStyle;
+    QString penCapStyle;
+    QString penJoinStyle;
+    QString brushColor;
+    QString brushStyle;
+    QString textString;
+    QString textColor;
+    QString textAlignment;
+    int textPointSize;
+    QString textFontFamily;
+    QString textFontStyle;
+    QString textFontWeight;
+};
 
-    struct Data {
-        int shapeID;
-        QString shapeType;
-        int shapeDimensions[10];
-        QString penColor;
-        int penWidth;
-        QString penStyle;
-        QString penCapStyle;
-        QString penJoinStyle;
-        QString brushColor;
-        QString brushStyle;
-        QString textString;
-        QString textColor;
-        QString textAlignment;
-        int textPointSize;
-        QString textFontFamily;
-        QString textFontStyle;
-        QString textFontWeight;
-    };
+void sdParser(QString filename) {
 
     Data blockData[BLOCK_MAX];
     QFile file(filename);
@@ -53,112 +53,388 @@ void sdParser(QString filename) {
 
     //Parses data
     int blockTrack = 0;
-    int lineTrack = 0;
     while (!stream.atEnd()) {
         QString line = stream.readLine();
 
+        //Reads ShapeId to the data struct
         if (line.startsWith("ShapeId:")) {
             line = line.mid(9);
             blockData[blockTrack].shapeID = line.toInt();
             line = file.readLine();
         }
 
-        else if (line.startsWith("ShapeType:")) {
-            line = line.mid(11);
-            blockData[blockTrack].shapeType = line;
-            line = file.readLine();
+        //Where the magic happens, detects that line starts with ShapeType
+        //Enters ShapeType statement, but does not enter any of the specific
+        //shape statements.
+        if (line.startsWith("ShapeType: Line")) {
+            qInfo() << "Reading line data...";
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+            else if (line.startsWith("PenColor:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penColor = line;
+                line = file.readLine();
+            }
+            else if (line.startsWith("PenWidth:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penWidth = line.toInt();
+            }
+            else if (line.startsWith("PenStyle:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penStyle = line;
+                line = file.readLine();
+            }
+            else if (line.startsWith("PenCapStyle:")) {
+                line = line.mid(13);
+                blockData[blockTrack].penCapStyle = line;
+                line = file.readLine();
+            }
+            //Won't read this for some reason
+            else if (line.startsWith("PenJoinStyle:")) {
+                line = line.mid(14);
+                blockData[blockTrack].penJoinStyle = line;
+                line = file.readLine();
+            }
+            blockTrack++;
         }
 
-        //Needs to read in up to 10 integers separated by commas into array
-        //Use a loop?
-        else if (line.startsWith("ShapeDimensions:")) {
-            line = line.mid(17);
-            blockData[blockTrack].shapeDimensions[1] = line.toInt();
-            line = file.readLine();
+        else if (line.startsWith("ShapeType: Polyline")) {
+            qInfo() << "Reading polyline data...";
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+            else if (line.startsWith("PenColor:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penColor = line;
+                line = file.readLine();
+            }
+            else if (line.startsWith("PenWidth:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penWidth = line.toInt();
+            }
+            else if (line.startsWith("PenStyle:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penStyle = line;
+                line = file.readLine();
+            }
+            else if (line.startsWith("PenCapStyle:")) {
+                line = line.mid(13);
+                blockData[blockTrack].penCapStyle = line;
+                line = file.readLine();
+            }
+            //Won't read this for some reason
+            else if (line.startsWith("PenJoinStyle:")) {
+                line = line.mid(14);
+                blockData[blockTrack].penJoinStyle = line;
+                line = file.readLine();
+            }
+            blockTrack++;
         }
 
-        else if (line.startsWith("PenColor:")) {
-            line = line.mid(10);
-            blockData[blockTrack].penColor = line;
-            line = file.readLine();
+        else if (line.startsWith("ShapeType: Polygon")) {
+            qInfo() << "Reading polygon data...";
+            //Needs to read in up to 10 integers separated by commas into array
+            //Use a loop?
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenColor:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenWidth:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penWidth = line.toInt();
+            }
+
+            else if (line.startsWith("PenStyle:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenCapStyle:")) {
+                line = line.mid(13);
+                blockData[blockTrack].penCapStyle = line;
+                line = file.readLine();
+            }
+
+            //Won't read this for some reason
+            else if (line.startsWith("PenJoinStyle:")) {
+                line = line.mid(14);
+                blockData[blockTrack].penJoinStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushColor:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushStyle:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushStyle = line;
+                line = file.readLine();
+            }
+            blockTrack++;
         }
 
-        else if (line.startsWith("PenWidth:")) {
-            line = line.mid(10);
-            blockData[blockTrack].penWidth = line.toInt();
+        else if (line.startsWith("ShapeType: Rectangle")) {
+            qInfo() << "Reading rectangle data...";
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenColor:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenWidth:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penWidth = line.toInt();
+            }
+
+            else if (line.startsWith("PenStyle:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenCapStyle:")) {
+                line = line.mid(13);
+                blockData[blockTrack].penCapStyle = line;
+                line = file.readLine();
+            }
+
+            //Won't read this for some reason
+            else if (line.startsWith("PenJoinStyle:")) {
+                line = line.mid(14);
+                blockData[blockTrack].penJoinStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushColor:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushStyle:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushStyle = line;
+                line = file.readLine();
+            }
         }
 
-        else if (line.startsWith("PenStyle:")) {
-            line = line.mid(10);
-            blockData[blockTrack].penStyle = line;
-            line = file.readLine();
+        else if (line.startsWith("ShapeType: Square")) {
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenColor:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenWidth:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penWidth = line.toInt();
+            }
+
+            else if (line.startsWith("PenStyle:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenCapStyle:")) {
+                line = line.mid(13);
+                blockData[blockTrack].penCapStyle = line;
+                line = file.readLine();
+            }
+
+            //Won't read this for some reason
+            else if (line.startsWith("PenJoinStyle:")) {
+                line = line.mid(14);
+                blockData[blockTrack].penJoinStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushColor:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushStyle:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushStyle = line;
+                line = file.readLine();
+            }
+            blockTrack++;
         }
 
-        else if (line.startsWith("PenCapStyle:")) {
-            line = line.mid(13);
-            blockData[blockTrack].penCapStyle = line;
-            line = file.readLine();
+        else if (line.startsWith("ShapeType: Ellipse")) {
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenColor:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenWidth:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penWidth = line.toInt();
+            }
+
+            else if (line.startsWith("PenStyle:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenCapStyle:")) {
+                line = line.mid(13);
+                blockData[blockTrack].penCapStyle = line;
+                line = file.readLine();
+            }
+
+            //Won't read this for some reason
+            else if (line.startsWith("PenJoinStyle:")) {
+                line = line.mid(14);
+                blockData[blockTrack].penJoinStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushColor:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushStyle:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushStyle = line;
+                line = file.readLine();
+            }
+            blockTrack++;
         }
 
-        //Won't read this for some reason
-        else if (line.startsWith("PenJoinStyle:")) {
-            line = line.mid(14);
-            blockData[blockTrack].penJoinStyle = line;
-            line = file.readLine();
+        else if (line.startsWith("ShapeType: Circle")) {
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenColor:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenWidth:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penWidth = line.toInt();
+            }
+
+            else if (line.startsWith("PenStyle:")) {
+                line = line.mid(10);
+                blockData[blockTrack].penStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("PenCapStyle:")) {
+                line = line.mid(13);
+                blockData[blockTrack].penCapStyle = line;
+                line = file.readLine();
+            }
+
+            //Won't read this for some reason
+            else if (line.startsWith("PenJoinStyle:")) {
+                line = line.mid(14);
+                blockData[blockTrack].penJoinStyle = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushColor:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushColor = line;
+                line = file.readLine();
+            }
+
+            else if (line.startsWith("BrushStyle:")) {
+                line = line.mid(12);
+                blockData[blockTrack].brushStyle = line;
+                line = file.readLine();
+            }
+            blockTrack++;
         }
 
-        else if (line.startsWith("BrushColor:")) {
-            line = line.mid(12);
-            blockData[blockTrack].brushColor = line;
-            line = file.readLine();
-        }
+        else if (line.startsWith("ShapeType: Text")) {
+            if (line.startsWith("ShapeDimensions:")) {
+                line = line.mid(17);
+                blockData[blockTrack].shapeDimensions[1] = line.toInt();
+                line = file.readLine();
+            }
+            else if (line.startsWith("TextString:")) {
+                line = line.mid(12);
+                blockData[blockTrack].textString = line;
+                line = file.readLine();
+            }
 
-        else if (line.startsWith("BrushStyle:")) {
-            line = line.mid(12);
-            blockData[blockTrack].brushStyle = line;
-            line = file.readLine();
-        }
+            else if (line.startsWith("TextColor:")) {
+                line = line.mid(11);
+                blockData[blockTrack].textColor = line;
+                line = file.readLine();
+            }
 
-        else if (line.startsWith("TextString:")) {
-            line = line.mid(12);
-            blockData[blockTrack].textString = line;
-            line = file.readLine();
-        }
+            else if (line.startsWith("TextPointSize:")) {
+                line = line.mid(15);
+                blockData[blockTrack].textPointSize = line.toInt();
+                line = file.readLine();
+            }
 
-        else if (line.startsWith("TextColor:")) {
-            line = line.mid(11);
-            blockData[blockTrack].textColor = line;
-            line = file.readLine();
-        }
+            else if (line.startsWith("TextFontFamily:")) {
+                line = line.mid(16);
+                blockData[blockTrack].textFontFamily = line;
+                line = file.readLine();
+            }
 
-        else if (line.startsWith("TextPointSize:")) {
-            line = line.mid(15);
-            blockData[blockTrack].textPointSize = line.toInt();
-            line = file.readLine();
-        }
+            else if (line.startsWith("TextFontStyle:")) {
+                line = line.mid(15);
+                blockData[blockTrack].textFontStyle = line;
+                line = file.readLine();
+            }
 
-        else if (line.startsWith("TextFontFamily:")) {
-            line = line.mid(16);
-            blockData[blockTrack].textFontFamily = line;
-            line = file.readLine();
-        }
-
-        else if (line.startsWith("TextFontStyle:")) {
-            line = line.mid(15);
-            blockData[blockTrack].textFontStyle = line;
-            line = file.readLine();
-        }
-
-        else if (line.startsWith("TextFontWeight:")) {
-            line = line.mid(16);
-            blockData[blockTrack].textFontWeight = line;
-            line = file.readLine();
-        }
-        lineTrack++;
-
-        if (lineTrack == 8) {
-            lineTrack = 0;
-            qInfo() << "Data for Block " << blockTrack << " parsed...";
+            else if (line.startsWith("TextFontWeight:")) {
+                line = line.mid(16);
+                blockData[blockTrack].textFontWeight = line;
+                line = file.readLine();
+            }
             blockTrack++;
         }
     }
