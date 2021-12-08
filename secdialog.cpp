a04c5f7729e50be4ebe345ad7ef2a59f6f824d5f
adit <92764174+SubtractIt@users.noreply.github.com>
2021-12-08 15:50:40 -0800

    Update secdialog.cpp

--- secdialog.cpp
+++ secdialog.cpp
@@ -13,16 +13,24 @@ secDialog::~secDialog()
     delete ui;
 }
 
-void secDialog::on_pushButton_clicked()
+void secDialog::on_pushButton_clicked()         //line
 {
     int xpos1 = ui->spinBox_line_firstX->text().toInt();
     int ypos1 = ui->spinBox_line_firstY->text().toInt();
     int xpos2 = ui->spinBox_line_secondX->text().toInt();
     int ypos2 = ui->spinBox_line_secondY->text().toInt();
 
-    Shape *rectangle = new Rectangle(xpos1, ypos1, xpos2, ypos2);
+    QPoint point1;
+    QPoint point2;
+    point1.setX(xpos1);
+    point1.setY(ypos1);
+    point2.setX(xpos2);
+    point2.setY(ypos2);
 
-    ui->widget->addShape(rectangle);
+
+    Shape *line = new Line(point1, point2);
+
+    ui->widget->addShape(line);
 
     ui->widget->update();
 }
